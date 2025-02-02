760e5a14c33c2f5edf35040ad59a148a299c8750
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-02 18:16:52 +0530

    Add signUpSchema with username validation to signUpSchema.ts

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -5,3 +5,6 @@ export const usernameValidation = z
     .min(2,"Username must be atleast 2 characters")
     .max(20,"Username Must not exceed more then 20 characters")
     .regex(/^[a-zA-Z0-9_]+$/,"Username Must Not Contain Special Character")
+
+
+    export const signUpSchema = z.object({usernameValidation})
