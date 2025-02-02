01f087faf984933e321f1a6eabe35d646529d393
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-02 18:15:46 +0530

    Add zod validation for username and update package dependencies

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,7 @@
+import {z} from "zod"
+
+export const usernameValidation = z
+    .string()
+    .min(2,"Username must be atleast 2 characters")
+    .max(20,"Username Must not exceed more then 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/,"Username Must Not Contain Special Character")
