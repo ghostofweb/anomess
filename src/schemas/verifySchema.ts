5cc0f6bd51f1e92add50a4c9e24224b409948fd5
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-02-02 18:52:35 +0530

    Fix typo in verification code message in verifySchema

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,5 +1,5 @@
 import {z} from 'zod'
 
 export const verifySchema = z.object({
-    code: z.string().length(6,{message:"Verification code Must be 6 Digit"})
+    code: z.string().length(6,{message:"Verification code Must be 6 Digits"})
 })
